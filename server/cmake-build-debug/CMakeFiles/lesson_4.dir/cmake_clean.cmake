35e3b3751df3b6d8da74cad993e6a30968f43c84
Sidorenko Danil <43047901+danilden1@users.noreply.github.com>
2018-10-01 13:49:19 +0300

    try to do server

---
+++ server/cmake-build-debug/CMakeFiles/lesson_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lesson_4.dir/sd_main.c.obj"
+  "lesson_4.pdb"
+  "lesson_4.exe"
+  "lesson_4.exe.manifest"
+  "liblesson_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lesson_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
